e1d99232ac6137d11f125468f5e305261f314771
Emerson Araujo <emersonsantos@gmail.com>
2023-02-17 00:45:54 -0300

    feat: add GetUsersControllers

---
+++ src/controllers/get-users/get-users.ts
@@ -0,0 +1,19 @@
+import { IGetUsersControllers, IGetUsersRepository } from './protocols';
+
+export class GetUsersControllers implements IGetUsersControllers {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+  async handle() {
+    try {
+      const users = await this.getUsersRepository.getUsers();
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: 'Somenthing went wrong.',
+      };
+    }
+  }
+}
