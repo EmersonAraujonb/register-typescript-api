732fb47ff44976b35283d65a26664fb739cf80b6
Emerson Araujo <emersonsantos@gmail.com>
2023-03-04 11:57:43 -0300

    feat: refactor update-city

--- src/controllers/city/update-city/update-city.ts
+++ src/controllers/city/update-city/update-city.ts
@@ -1,37 +1,29 @@
 import { City } from '../../../models/city';
 import { HttpResquest, HttpResponse } from '../../protocols';
-import { IUpdateCityController, IUpdateCityRepository, UpdateCityParams } from './protocols';
+import {
+  IUpdateCityController,
+  IUpdateCityRepository,
+  UpdateCityParams,
+} from './protocols';
 
 export class UpdateCityController implements IUpdateCityController {
-  constructor(private readonly updateCityRepository: IUpdateCityRepository){}
-    async handle(httpResquest: HttpResquest<any>): Promise<HttpResponse<City>> {
-    const id = httpResquest?.params?.id;
-    const body = httpResquest?.body;
+  constructor(private readonly updateCityRepository: IUpdateCityRepository) {}
+  async handle(httpResquest: HttpResquest<any>): Promise<HttpResponse<City>> {
     try {
+      const id = httpResquest?.params?.id;
+      const body = httpResquest?.body;
+      
       if (!id) {
         return {
           statusCode: 400,
           body: 'Missing city id',
         };
       }
-      const allowedFieldToUpdate: (keyof UpdateCityParams)[] = [
-        'city',
-        'state',
-      ];
-      const someFieldIsNotAllowedToUpdate = Object.keys(body).some(
-        (key) => !allowedFieldToUpdate.includes(key as keyof UpdateCityParams)
-      );
-      if (someFieldIsNotAllowedToUpdate) {
-        return {
-          statusCode: 400,
-          body: 'Some received field is not allowed',
-        };
-      }
-      const city = await this.updateCityRepository.updateCity(id, body)
+      const city = await this.updateCityRepository.updateCity(id, body);
       return {
         statusCode: 200,
-        body: city 
-    }
+        body: city,
+      };
     } catch (error) {
       return {
         statusCode: 500,
